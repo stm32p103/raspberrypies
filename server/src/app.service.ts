1fba66ea964f39e357ce33ada7cb013a2139d3b3
stm32p103 <stm32p103@gmail.com>
2019-07-28 22:16:23 +0900

    なんちゃってCIで変更後

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello World! Modified(2).';
+        return 'Hello World! Modified(3).';
     }
 }
