ea9be85a1f21579d7b83af3f93ee27cc7531992a
stm32p103 <stm32p103@gmail.com>
2019-07-28 22:13:11 +0900

    なんちゃってCI用の簡易トリガ

--- trigger/index.js
+++ trigger/index.js
@@ -1,16 +1,41 @@
 const http = require( 'http' );
 const spawn = require('child_process').spawn;
+const header = { 'Content-Type': 'text/plain' };
 
-var prc = spawn( 'notepad' );
+// git pullする。spawnは spawn( 'git pull' )してはならない。エラーになる。
+function executePull() {
+	const prc = spawn( 'git', [ 'pull' ] );
+	prc.stdout.setEncoding( 'utf8' );
+	prc.stderr.setEncoding( 'utf8' );
+	
+	return new Promise( ( resolve, reject ) => {
+		prc.once( 'exit', code => { 
+			resolve( code );
+		} );
+		prc.once( 'error', err => { 
+			reject( err )
+		} );
+	} );
+}
 
-//noinspection JSUnresolvedFunction
-prc.stdout.setEncoding('utf8');
-prc.stdout.on('data', function (data) {
-    var str = data.toString()
-    var lines = str.split(/(\r?\n)/g);
-    console.log(lines.join(""));
-});
+const app = http.createServer( ( req, res ) => {
+	let message = '';
+	
+	executePull().then( ( code ) => {
+		// 成功した場合
+		res.writeHead( 200, header );
+		message = `Pull succeeded. (code:${code})`;
+	} ).catch( ( err ) => {
+		// 失敗した場合
+		res.writeHead( 405, header );
+		message = `Pull failed. (code:${err})`;
+	} ).finally( () => {
+		console.log( message );
+		// 応答を返す
+		res.write( message );
+		res.end();
+	} );
+} );
 
-prc.on('close', function (code) {
-    console.log('process exit code ' + code);
-});
+// ポートは適当に変える
+app.listen( 4000 );
