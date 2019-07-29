81f4204f62cbe1284885cef5bd7243c267d57544
stm32p103 <stm32p103@gmail.com>
2019-07-29 11:35:41 +0900

    PC/SC sample

---
+++ pcsc/dist/main.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nfc_pcsc_1 = require("nfc-pcsc");
+const nfc = new nfc_pcsc_1.NFC();
+nfc.on('reader', reader => {
+    console.log(`${reader.reader.name}  device attached`);
+    reader.on('card', card => {
+        console.log(`${reader.reader.name}  card detected`, card);
+    });
+    reader.on('card.off', card => {
+        console.log(`${reader.reader.name}  card removed`, card);
+    });
+    reader.on('error', err => {
+        console.log(`${reader.reader.name}  an error occurred`, err);
+    });
+    reader.on('end', () => {
+        console.log(`${reader.reader.name}  device removed`);
+    });
+});
+nfc.on('error', err => {
+    console.log('an error occurred', err);
+});
+//# sourceMappingURL=main.js.map
